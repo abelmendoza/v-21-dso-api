fbd973334dd0503aca13b338977abafb3f812d69
ABEL MENDOZA <mendozaabel66@gmail.com>
2021-06-16 17:29:55 -0400

    Added DBContext and controller to person

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiEstudiante.Context;
+
+namespace ApiEstudiante.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public PersonaController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            //Error handling Try-catch
+
+            return Ok(context.persona.ToList());
+        }
+    }
+}
