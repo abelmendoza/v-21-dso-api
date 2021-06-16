2139f11621e80b15f50ccd6c3d4ce58178deb2c0
ABEL MENDOZA <mendozaabel66@gmail.com>
2021-06-16 17:55:57 -0400

    Added function to GetById and Post for saving person

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiEstudiante.Context;
+using ApiEstudiante.Models;
 
 namespace ApiEstudiante.Controllers
 {
@@ -21,8 +22,42 @@ namespace ApiEstudiante.Controllers
         public ActionResult GetAll()
         {
             //Error handling Try-catch
-
-            return Ok(context.persona.ToList());
+            try
+            {
+                return Ok(context.persona.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        //api/v1/persona/12563
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult GetById(int id)
+        {
+            try
+            {
+                var persona = context.persona.FirstOrDefault(persona => persona.id == id);
+                return Ok(persona);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Persona persona)
+        {
+            try
+            {
+                context.persona.Add(persona);
+                context.SaveChanges();
+                return CreatedAtRoute("GetById", new { persona.id }, persona);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
