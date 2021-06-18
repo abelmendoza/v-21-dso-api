5d9d85991179092282fdf50c58e601e60025824e
ABEL MENDOZA <mendozaabel66@gmail.com>
2021-06-18 17:19:41 -0400

    Added to CarreraController, EstudianteController

---
+++ Controllers/EstudianteController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiEstudiante.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEstudiante.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EstudianteController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public EstudianteController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(_context.estudiante.Include(e => e.persona).Include(e => e.carrera).ToList());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
--- Models/Estudiante.cs
+++ Models/Estudiante.cs
@@ -11,7 +11,9 @@ namespace ApiEstudiante.Models
         [Key]
         public int id { get; set; }
         public int personaId { get; set; }
+        public Persona persona { get; set; }
         public int carreraId { get; set; }
+        public Carrera carrera { get; set; }
         public string matricula { get; set; }
         public bool estado { get; set; }
 
